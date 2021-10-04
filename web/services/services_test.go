99b9cb2f02136586e4152aaa76567446c03b7ff9
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-10-04 12:43:28 +0200

    na cifra

--- web/services/services_test.go
+++ web/services/services_test.go
@@ -1,12 +1,19 @@
 package services
 
 import (
+	"fmt"
+
+	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func setupTestDatabase() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres port=32432 dbname=trento_test sslmode=disable"
+	viper.AutomaticEnv()
+	viper.SetDefault("dbhost", "localhost")
+	viper.SetDefault("dbport", "32432")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=postgres password=postgres dbname=trento_test sslmode=disable", viper.GetString("dbhost"), viper.GetString("dbport"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
