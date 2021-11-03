c5e02faf099ae0432ba44a8247d4acef2fdc5a98
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-11-03 13:09:41 +0100

    Add hosts projector tests

---
+++ agent/discovery/mocks/discovered_sap_system_mock.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/trento-project/trento/internal/sapsystem"
+)
+
+func NewDiscoveredSAPSystemMock() sapsystem.SAPSystemsList {
+	var s sapsystem.SAPSystemsList
+
+	jsonFile, err := os.Open("./test/fixtures/discovery/sap_system/sap_system_discovery.json")
+	if err != nil {
+		panic(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &s)
+
+	return s
+}
