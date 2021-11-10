b89e7bdc64e1c8df54295dc01a50bcd7528db197
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-11-10 19:35:59 +0100

    Skip foreign key constraint when automigrating

--- internal/db/db.go
+++ internal/db/db.go
@@ -23,7 +23,13 @@ func InitDB(config *Config) (*gorm.DB, error) {
 		config.Password,
 		config.DBName)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// TODO: a trade-off of dealing with eventual consistency is to not enforce foreign key constraints
+	// at database level.
+	// We might not want to do this at a global level, but for now we don't have migrations other than
+	// automigrations and this is just good enough for our needs.
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
 		return nil, err
 	}
