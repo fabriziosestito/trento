d918e0efec32d05b4f8edb32bb2ee318c305120d
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-11-16 17:36:33 +0100

    WIP

--- web/datapipeline/clusters_projector.go
+++ web/datapipeline/clusters_projector.go
@@ -4,11 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/trento-project/trento/internal/cluster"
+	"github.com/trento-project/trento/internal/cluster/cib"
 	"github.com/trento-project/trento/web/entities"
 	"github.com/trento-project/trento/web/models"
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -49,14 +53,17 @@ func transformClusterData(cluster *cluster.Cluster) (*entities.Cluster, error) {
 		return nil, fmt.Errorf("no cluster ID found")
 	}
 
+	clusterDetail, _ := parseClusterDetail(cluster)
+	fmt.Println(clusterDetail)
 	return &entities.Cluster{
 		ID:          cluster.Id,
 		Name:        cluster.Name,
 		ClusterType: detectClusterType(cluster),
 		// TODO: Cost-optimized has multiple SIDs, we will need to implement this in the future
-		SIDs:            getHanaSIDs(cluster),
+		SIDs:            getClusterSIDs(cluster),
 		ResourcesNumber: cluster.Crmmon.Summary.Resources.Number,
 		HostsNumber:     cluster.Crmmon.Summary.Nodes.Number,
+		Detail:          (datatypes.JSON)(clusterDetail),
 	}, nil
 }
 
@@ -87,9 +94,9 @@ func detectClusterType(cluster *cluster.Cluster) string {
 	}
 }
 
-// getHanaSIDs returns the SIDs of the cluster
+// getClusterSIDs returns the SIDs of the cluster
 // TODO: HANA scale-out has multiple SIDs, we will need to implement this in the future
-func getHanaSIDs(c *cluster.Cluster) []string {
+func getClusterSIDs(c *cluster.Cluster) []string {
 	var sids []string
 	for _, r := range c.Cib.Configuration.Resources.Clones {
 		if r.Primitive.Type == "SAPHanaTopology" {
@@ -102,4 +109,202 @@ func getHanaSIDs(c *cluster.Cluster) []string {
 	}
 
 	return sids
+}
+
+func parseClusterDetail(c *cluster.Cluster) (json.RawMessage, error) {
+	switch detectClusterType(c) {
+	case models.ClusterTypeScaleUp:
+		fallthrough
+	case models.ClusterTypeScaleOut:
+		return parseClusterDetailHANA(c)
+	default:
+		return json.RawMessage{}, nil
+	}
+}
+
+func parseClusterDetailHANA(c *cluster.Cluster) (json.RawMessage, error) {
+	nodes := parseClusterNodes(c)
+
+	var sid string
+	var systemReplicationMode, systemReplicationOperationMode, secondarySyncState, srHealthState string
+
+	sids := getClusterSIDs(c)
+	if len(sids) > 0 {
+		sid = sids[0]
+	}
+
+	if len(nodes) > 0 {
+		systemReplicationMode, _ = getHANAAttribute(nodes[0], "srmode", sid)
+		systemReplicationOperationMode, _ = getHANAAttribute(nodes[0], "op_mode", sid)
+		secondarySyncState = getHANASecondarySyncState(nodes, sid)
+		srHealthState = getHANAHealthState(nodes[0], sid)
+	}
+
+	dateLayout := "Mon Jan 2 15:04:05 2006"
+	cibLastWritten, _ := time.Parse(dateLayout, c.Crmmon.Summary.LastChange.Time)
+
+	clusterDetail := &entities.ClusterDetailHANA{
+		SystemReplicationMode:          systemReplicationMode,
+		SecondarySyncState:             secondarySyncState,
+		SystemReplicationOperationMode: systemReplicationOperationMode,
+		SRHealthState:                  srHealthState,
+		CIBLastWritten:                 cibLastWritten,
+		StonithType:                    getClusterFencingType(c),
+		StoppedResources:               getClusterStoppedResources(c),
+		Nodes:                          nodes,
+	}
+	fmt.Println(clusterDetail)
+	return json.Marshal(clusterDetail)
+}
+
+func parseClusterNodes(c *cluster.Cluster) []*entities.ClusterNode {
+	var nodes []*entities.ClusterNode
+
+	// TODO: remove plain resources grouping as in the future we'll need to distinguish between Cloned and Groups
+	resources := c.Crmmon.Resources
+	for _, g := range c.Crmmon.Groups {
+		resources = append(resources, g.Resources...)
+	}
+
+	for _, c := range c.Crmmon.Clones {
+		resources = append(resources, c.Resources...)
+	}
+
+	for _, n := range c.Crmmon.NodeAttributes.Nodes {
+		node := &entities.ClusterNode{
+			Name:       n.Name,
+			Attributes: make(map[string]string),
+		}
+
+		for _, a := range n.Attributes {
+			node.Attributes[a.Name] = a.Value
+		}
+
+		for _, r := range resources {
+			if r.Node.Name == n.Name {
+				resource := &entities.ClusterNodeResource{
+					Id:   r.Id,
+					Type: r.Agent,
+					Role: r.Role,
+				}
+
+				switch {
+				case r.Active:
+					resource.Status = "active"
+				case r.Blocked:
+					resource.Status = "blocked"
+				case r.Failed:
+					resource.Status = "failed"
+				case r.FailureIgnored:
+					resource.Status = "failure_ignored"
+				case r.Orphaned:
+					resource.Status = "orphaned"
+				}
+
+				var primitives []cib.Primitive
+				primitives = append(primitives, c.Cib.Configuration.Resources.Primitives...)
+
+				for _, g := range c.Cib.Configuration.Resources.Groups {
+					primitives = append(primitives, g.Primitives...)
+				}
+
+				if r.Agent == "ocf::heartbeat:IPaddr2" {
+					for _, p := range primitives {
+						if r.Id == p.Id {
+							if len(p.InstanceAttributes) > 0 {
+								node.VirtualIPs = append(node.VirtualIPs, p.InstanceAttributes[0].Value)
+								break
+							}
+						}
+					}
+				}
+
+				for _, nh := range c.Crmmon.NodeHistory.Nodes {
+					if nh.Name == n.Name {
+						for _, rh := range nh.ResourceHistory {
+							if rh.Name == resource.Id {
+								resource.FailCount = rh.FailCount
+								break
+							}
+						}
+					}
+				}
+
+				node.Resources = append(node.Resources, resource)
+			}
+		}
+		nodes = append(nodes, node)
+	}
+
+	return nodes
+}
+
+func getHANAAttribute(node *entities.ClusterNode, attributeName string, sid string) (string, bool) {
+	hanaAttributeName := fmt.Sprintf("hana_%s_%s", strings.ToLower(sid), attributeName)
+	value, ok := node.Attributes[hanaAttributeName]
+
+	return value, ok
+}
+
+func getHANASecondarySyncState(nodes []*entities.ClusterNode, sid string) string {
+	for _, n := range nodes {
+		if getHANAStatus(n, sid) == "Secondary" {
+			if s, ok := getHANAAttribute(n, "sync_state", sid); ok {
+				return s
+			}
+		}
+	}
+	return ""
+}
+
+// HANAStatus parses the hana_<SID>_roles string and returns the SAPHanaSR Health state
+// Possible values: Primary, Secondary
+// e.g. 4:P:master1:master:worker:master returns Primary (second element)
+func getHANAStatus(node *entities.ClusterNode, sid string) string {
+	if r, ok := getHANAAttribute(node, "roles", sid); ok {
+		status := strings.SplitN(r, ":", 3)[1]
+
+		switch status {
+		case "P":
+			return "Primary"
+		case "S":
+			return "Secondary"
+		}
+	}
+	return ""
+}
+
+func getHANAHealthState(node *entities.ClusterNode, sid string) string {
+	if r, ok := getHANAAttribute(node, "roles", sid); ok {
+		healthState := strings.SplitN(r, ":", 2)[0]
+		return healthState
+	}
+	return ""
+}
+
+func getClusterFencingType(c *cluster.Cluster) string {
+	const stonithAgent string = "stonith:"
+	const stonithResourceMissing string = "notconfigured"
+
+	for _, resource := range c.Crmmon.Resources {
+		if strings.HasPrefix(resource.Agent, stonithAgent) {
+			return strings.Split(resource.Agent, ":")[1]
+		}
+	}
+	return stonithResourceMissing
+}
+
+func getClusterStoppedResources(c *cluster.Cluster) []*entities.ClusterNodeResource {
+	var stoppedResources []*entities.ClusterNodeResource
+
+	for _, r := range c.Crmmon.Resources {
+		if r.NodesRunningOn == 0 && !r.Active {
+			resource := &entities.ClusterNodeResource{
+				Id: r.Id,
+			}
+			stoppedResources = append(stoppedResources, resource)
+		}
+	}
+
+	return stoppedResources
 }
--- web/entities/cluster.go
+++ web/entities/cluster.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lib/pq"
 	"github.com/trento-project/trento/web/models"
+	"gorm.io/datatypes"
 )
 
 type Cluster struct {
@@ -14,8 +15,36 @@ type Cluster struct {
 	SIDs            pq.StringArray `gorm:"column:sids; type:text[]"`
 	ResourcesNumber int
 	HostsNumber     int
-	Tags            []models.Tag `gorm:"polymorphic:Resource;polymorphicValue:clusters"`
+	Tags            []*models.Tag `gorm:"polymorphic:Resource;polymorphicValue:clusters"`
 	UpdatedAt       time.Time
+	Hosts           []*Host        `gorm:"foreignkey:cluster_id"`
+	Detail          datatypes.JSON `json:"payload" binding:"required"`
+}
+
+type ClusterDetailHANA struct {
+	SystemReplicationMode          string                 `json:"system_replication_mode"`
+	SystemReplicationOperationMode string                 `json:"system_replication_operation_mode"`
+	SecondarySyncState             string                 `json:"secondary_sync_state"`
+	SRHealthState                  string                 `json:"sr_health_state"`
+	CIBLastWritten                 time.Time              `json:"cib_last_written"`
+	StonithType                    string                 `json:"stonith_type"`
+	StoppedResources               []*ClusterNodeResource `json:"stopped_resources"`
+	Nodes                          []*ClusterNode         `json:"nodes"`
+}
+
+type ClusterNode struct {
+	Name       string                 `json:"name"`
+	Attributes map[string]string      `json:"attributes"`
+	Resources  []*ClusterNodeResource `json:"resources"`
+	VirtualIPs []string               `json:"virtual_ips"`
+}
+
+type ClusterNodeResource struct {
+	Id        string `json:"id"`
+	Type      string `json:"type"`
+	Role      string `json:"role"`
+	Status    string `json:"status"`
+	FailCount int    `json:"fail_count"`
 }
 
 func (h *Cluster) ToModel() *models.Cluster {
